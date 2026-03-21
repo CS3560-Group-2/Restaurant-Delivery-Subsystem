5ad48c3fdb6a21e4943b6163bdd064ec094346bd
JonathanBarsoum <102571215+JonathanBarsoum@users.noreply.github.com>
2026-03-20 17:13:24 -0700

    Add files via upload

---
+++ methods.py
@@ -0,0 +1,23 @@
+def placeOrder():
+	order = []      #creates an empty order
+	item = ""       #creates an empty item
+	while True:     #
+	    item = input("what would you like to order: ")
+	    if item == "":
+               break
+            order.append(item)
+        return order
+
+def maketicket(cID, rID, dID, oID)
+	return ticket
+
+def foodIsDelivered():
+	status.update(True)
+        ticket.close()
+	return
+
+def isFoodDelivered():
+	status.display()
+	return 0
+
+
