ed3d25768ce545034f7578555129b83a50795c2b
therealsnooze <carlosdsanchez06@gmail.com>
2026-04-24 16:02:50 -0700

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -163,3 +163,19 @@ class Customer(User):
       order.delivery_instructions = instructions
       return True
     return False
+
+class PaymentMethod:
+  """
+  represent's a customer's payment method
+  allows for multiple payment options per customer
+  """
+
+  def __init__(self, payment_id: int, card_number: str, card_type: str):
+    # identifier for payment method
+    self.payment_id = payment_id
+    
+    # card number is stored as string to avoid potential indexing issues
+    self.card_number = card_number
+
+    # card can be either credit or debit
+    self.card_type = card_type
