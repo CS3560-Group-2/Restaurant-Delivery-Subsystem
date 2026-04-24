d804bca3c93481e60df6c1690742ea202319d79f
therealsnooze <carlosdsanchez06@gmail.com>
2026-04-24 13:59:28 -0700

    Update Restaurant.py
    added methods to restaurant class

--- Restaurant.py
+++ Restaurant.py
@@ -12,3 +12,31 @@ class Restaurant(User):
     # list of menu items offered
     self.menu: List[MenuItem] = []
 
+  def add_menu_item(self, item: MenuItem) -> None:
+    """
+    adds a new item to the restaurant's menu
+    """
+    self.menu.append(item)
+
+  def remove_menu_item(self, item_id: int) -> bool:
+    """
+    removes a menu item based on item ID
+    returns true if removal is successful
+    """
+    for item in self.menu
+      if item.item_id == item_id:
+        self.menu.remove(item)
+        return True
+    return False
+
+  def mark_order_ready(self, order) -> bool:
+    """
+    marks order for delivery
+    transitions order status from 'preparing' to ready
+    signals that it can be picked up by driver
+    """
+    if order.status == "preparing":
+      order.status = "ready
+      return True
+    return False
+  
