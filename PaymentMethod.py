d218f9f11bf6ffb167cd0a3757a4e7724b6bfd02
therealsnooze <carlosdsanchez06@gmail.com>
2026-04-24 14:25:00 -0700

    Update PaymentMethod.py

--- PaymentMethod.py
+++ PaymentMethod.py
@@ -4,7 +4,9 @@ class PaymentMethod:
   allows for multiple payment options per customer
   """
 
-  def __init__(self, payment_id: int, card_number: str, expiry: str):
-    self.payment_id = payment_id
+  def __init__(card_number: str, card_type: str):
+    # card number is stored as string to avoid potential indexing issues
     self.card_number = card_number
-    self.expiry = expiry
+
+    # card can be either credit or debit
+    self.card_type = card_type
