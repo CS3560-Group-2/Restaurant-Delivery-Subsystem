73dc23bcb900d7c4e9845662645fbc18d87ea898
therealsnooze <carlosdsanchez06@gmail.com>
2026-04-25 00:01:00 -0700

    Update Classes.py
    implemented more db functionality to most methods

--- Classes.py
+++ Classes.py
@@ -5,7 +5,7 @@ import mysql.connector
 con = mysql.connector.connect(
   user = 'root',
   host = 'localhost',
-  database = 'food_delivery' # placeholder name
+  database = 'food_delivery', # placeholder name
   passwd = 'password' # placeholder password
 )
 
@@ -26,10 +26,14 @@ class User:
     # indicates whether account is logged in
     self.logged_in = False
 
-def sign_up(self) -> bool:
+def sign_up(self, user_type: str) -> bool:
   """
   simulates account registration
   """
+  cur.execute("""
+              INSERT INTO users (UserID, Type, Name, Active)) VALUES (%s, %s, %s, %s)
+              """, (self.accountID, user_type, self.name, self.active))
+  con.commit()
   return True
 
 def sign_in(self) -> None:
@@ -48,7 +52,9 @@ def delete_account(self) -> None:
   """
   deactivates account
   """
-  self.active = false
+  self.active = False
+  cur.execute("""UPDATE Users SET Active = %s WHERE UserID = %s""", (False, self.accountID))
+  con.commit()
 
 def edit_profile(self, name: Optional[str] = None) -> None:
   """
@@ -56,6 +62,8 @@ def edit_profile(self, name: Optional[str] = None) -> None:
   """
   if name:
     self.name = name
+    cur.execute("""UPDATE Users SET Name = %s WHERE UserID = %s""", (self.name, self.accountID))
+    con.commit()
 
 class Restaurant(User):
   """
@@ -71,12 +79,35 @@ class Restaurant(User):
     # list of menu items offered
     self.menu: List[MenuItem] = []
 
+  def sign_up(self) -> bool:
+     """
+     inserts restaurant into DB
+     """
+     super().sign_up("Restaurant")
+
+     cur.execute("""INSERT INTO Restaurants (RestaurantID, Address) VALUES (%s, %s)""", (self.accountID, str(self.address)))
+     con.commit()
+
+     return True
+
   def add_menu_item(self, item: MenuItem) -> None:
     """
     adds a new item to the restaurant's menu
     """
     self.menu.append(item)
 
+    cur.execute("""INSERT INTO MenuItems (ItemID, Name, Price, RestaurantID) VALUES (%s, %s, %s, %s)""", (item.item_id, item.name, item.price, self.accountID))
+    con.commit()
+
+  def remove_menu_item(self, item_id: int) -> None:
+    """
+    removes a menu item from db based on ID
+    """
+    cur.execute("DELETE FROM MenuItems WHERE ItemID = %s", (item_id,))
+    con.commit()
+
+  # old remove_menu_item method, not currently in use
+  '''
   def remove_menu_item(self, item_id: int) -> bool:
     """
     removes a menu item based on item ID
@@ -87,6 +118,7 @@ class Restaurant(User):
         self.menu.remove(item)
         return True
     return False
+  '''
 
   def mark_order_ready(self, order) -> bool:
     """
@@ -96,6 +128,8 @@ class Restaurant(User):
     """
     if order.status == "preparing":
       order.status = "ready"
+      cur.execute("""UPDATE Orders SET Status = %s WHERE OrderID = %s""", ("ready", order.order_id))
+      con.commit()
       return True
     return False
 
@@ -119,6 +153,17 @@ class Driver(User):
 
         # the order currently assigned to the driver (if any)
         self.current_order = None
+    
+    def sign_up(self) -> bool:
+       """
+       inserts driver into DB
+       """
+       super().sign_up("Driver")
+
+       cur.execute("""INSERT INTO Drivers (DriverID, LicensePlate, Location, Status) VALUES (%s, %s, %s, %s)""", (self.accountID, self.license_plate, str(self.location), self.status))
+       con.commit()
+
+       return True
 
     def assign_order(self, order) -> bool:
         """
@@ -128,6 +173,9 @@ class Driver(User):
         if self.status == "available":
             self.current_order = order
             self.status = "assigned"
+            cur.execute("""UPDATE Orders SET DriverID = %s, Status = %s WHERE OrderID = %s""", (self.accountID, "delivering", order.order_id))
+            con.commit()
+            return True
         return False
 
     def update_location(self, location: Address) -> None:
@@ -135,6 +183,8 @@ class Driver(User):
         updates driver's current location
         """
         self.location = location
+        cur.execute("""UPDATE Drivers SET Location = %s WHERE DriverID = %s""", (str(location), self.accountID))
+        con.commit()
     
     def complete_delivery(self) -> None:
         """
@@ -143,6 +193,8 @@ class Driver(User):
         """
         if self.current_order:
             self.current_order.status = "delivered"
+            cur.execute("""UPDATE Orders SET Status = %s WHERE OrderID = %s""", ("delivered", self.current_order.order_id))
+            con.commit()
             self.current_order = None
             self.status = "available"
             return True
@@ -163,11 +215,25 @@ class Customer(User):
     # list of payment methods
     self.payment_methods: List[PaymentMethod] = []
 
+  def sign_up(self) -> bool:
+    """
+    insterts customer into DB
+    """
+    super().sign_up("Customer")
+  
+    cur.execute("""INSERT INTO Customers (CustomerID, Address) VALUES %s, %s)""", (self.accountID, str(self.address)))
+    con.commit()
+    
+    return True
+
+
   def add_payment_method(self, payment_method: PaymentMethod) -> None:
     """
     adds a new payment method to customer account
     """
     self.payment_methods.append(payment_method)
+    cur.execute("""INSERT INTO PaymentMethods (PaymentID, CardNumber, CardType, CustomerID)) VALUES (%s, %s, %s, %s)""", (payment_method.payment_id, payment_method.card_number, payment_method.card_type, self.accountID))
+    con.commit()
 
   def remove_payment_method(self, payment_method_id: int) -> bool:
     """
@@ -177,6 +243,8 @@ class Customer(User):
     for pm in self.payment_methods:
       if pm.payment_id == payment_method_id:
         self.payment_methods.remove(pm)
+        cur.execute("""DELETE FROM PaymentMethods WHERE PaymentID = %s""", (payment_method_id, self.accountID))
+        con.commit()
         return True
     return False
 
@@ -187,6 +255,8 @@ class Customer(User):
     """
     if order.status in ["placed", "preparing"]:
       order.delivery_instructions = instructions
+      cur.execute("""UPDATE Orders SET DeliveryInstructions = %s WHERE OrderID = %s""", (instructions, order.order_id))
+      con.commit()
       return True
     return False
 
@@ -232,12 +302,31 @@ class Order:
         # total price of order
         self.total_price = 0.0
 
+    def save_to_db(self) -> None:
+        """
+        inserts new order into database
+        """
+
+        cur.execute("""INSERT INTO Orders (OrderID, CustomerID, RestaurantID, Status, TotalPrice, Intructions) VALUES (%s, %s, %s, %s, %s, %s)""", (
+           self.order_id,
+           self.customer.accountID,
+           self.restaurant.accountID,
+           self.status,
+           self.total_price,
+           self.delivery_instructions
+        ))
+        con.commit()
+
     def add_item(self, item: MenuItem) -> None:
         """
         adds an item to the order and updates total price
         """
         self.items.append(item)
         self.total_price += item.price
+
+        cur.execute("""INSERT INTO OrderItems (OrderID, ItemID) VALUES (%s, %s)""", (self.order_id, item.item_id))
+        cur.execute("""UPDATE ORDERS SET TotalPrice = %s WHERE ORDERID = %s""", (self.total_price, self.order_id))
+        con.commit()
 
     def remove_item(self, item_id: int) -> bool:
         """
@@ -248,6 +337,9 @@ class Order:
             if item.item_id == item_id:
                 self.items.remove(item)
                 self.total_price -= item.price
+                cur.execute("""DELETE FROKM OrderItems WHERE OrderID = %s AND ItemID = %s""", (self.order_id, item_id))
+                cur.execute("""UPDATE ORDERS SET TOotalPrice = %s WHERE OrderID = %s""", (self.total_price, self.order_id))
+                con.commit()
                 return True
         return False
 
@@ -266,9 +358,27 @@ class Order:
             self.driver = driver
             driver.assign_order(self)
             self.status = "delivering"
+            cur.execute("""UPDATE Orders SET DriverID = %s, Status = %s WHERE OrderID = %s""", (
+               driver.accountID,
+               self.status,
+               self.order_id
+            ))
+            con.commit()
             return True
         return False
 
+    def update_status(self, new_status: str) -> None:
+        """
+        updates order status in databse
+        """
+
+        self.status = new_status
+
+        cur.execute("""UPDATE Orders SET Status = %s WHERE OrderID = %s""", (new_status, self.order_id))
+        con.commit()
+
+    # old update_status method, not currently in use
+    '''
     def update_status(self, new_status: str) -> bool:
         """
         safely updates order status with simple validation
@@ -286,6 +396,7 @@ class Order:
             self.status = new_status
             return True
         return False
+    '''
 
     def cancel_order(self) -> bool:
         """
@@ -293,10 +404,12 @@ class Order:
         """
         if self.status in ["placed", "preparing"]:
             self.status = "cancelled"
+            cur.execute("""UPDATE Orders SET STATUS = %s WHERE ORDERID = %s""", ("cancelled", self.order_id))
+            con.commit()
             return True
         return False
   
-  class Address:
+class Address:
     """
     represents a physical address used for delivery and location tracking
     now supports international addresses via country field
@@ -309,6 +422,20 @@ class Order:
         self.zip_code = zip_code
         self.country = country
         self.unit = unit  # apartment, suite, etc.
+
+    def save_to_db(self) -> None:
+        """
+        inserts address into database
+        """
+        cur.execute("""INSERT INTO Addresses (AddressID, Street, City, State, ZipCode, County) VALUES (%s, %s, %s, %s, %s, %s)""",
+                    (self.address_id,
+                     self.street,
+                     self.city,
+                     self.state,
+                     self.zip_code,
+                     self.country
+                    ))
+        con.commit()
 
     def __str__(self) -> str:
         """
@@ -351,6 +478,16 @@ class Order:
         if unit is not None:
             self.unit = unit
 
+        cur.execute("""UPDATE Addresses SET Street = %s, City = %s, State = %s, ZipCode = %s, Country = %s WHERE AddressID = %s""", (
+           street,
+           city,
+           state,
+           zip_code,
+           country,
+           self.address_id
+        ))
+        con.commit()
+
     def is_same_city(self, other_address) -> bool:
         """
         checks if another address is in the same city AND country
@@ -378,3 +515,76 @@ class Order:
             return 1.0
 
         return 5.0
+    
+class MenuItem:
+    """
+    represents a menu item offered by a restaurant
+    """
+
+    def __init__(self, item_id: int, name: str, price: float, restaurant_id: int):
+        self.item_id = item_id
+        self.name = name
+        self.price = price
+        self.restaurant_id = restaurant_id
+
+    def save_to_db(self) -> None:
+        """
+        inserts menu item into database
+        """
+        cur.execute("""INSERT INTO MenuItems (ItemID, Name, Price, RestaurantID) VALUES (%s, %s, %s, %s)""", (self.item_id, self.name, self.price, self.restaurant_id))
+        con.commit()
+
+    def update_price(self, new_price: float) -> None:
+        """
+        updates price in both memory and database
+        """
+        self.price = new_price
+
+        cur.execute("""UPDATE MenuItems SET Price = %s WHERE ItemID = %s""", (new_price, self.item_id))
+        con.commit()
+
+    def delete_from_db(self) -> None:
+        """
+        removes item from database
+        """
+        cur.execute("""DELETE FROM MenuItems WHERE ItemID = %s""", (self.item_id,))
+        con.commit() 
+
+class OrderItem:
+    """
+    represents a specific item within an order
+    """
+
+    def __init__(self, order_id: int, menu_item: MenuItem, quantity: int):
+        self.order_id = order_id
+        self.menu_item = menu_item
+        self.quantity = quantity
+
+    def save_to_db(self) -> None:
+        """
+        inserts order item into database
+        """
+        cur.execute("""INSERT INTO OrderItems (OrderID, ItemID, Quantity) VALUES (%s, %s, %s)""", (self.order_id, self.menu_item.item_id, self.quantity))
+        con.commit()
+
+    def update_quantity(self, new_quantity: int) -> None:
+        """
+        updates quantity in database
+        """
+        self.quantity = new_quantity
+
+        cur.execute("""UPDATE OrderItems SET Quantity = %s WHERE OrderID = %s AND ItemID = %s""", (new_quantity, self.order_id, self.menu_item.item_id))
+        con.commit()
+
+    def delete_from_db(self) -> None:
+        """
+        removes this item from the order
+        """
+        cur.execute("""DELETE FROM OrderItems WHERE OrderID = %s AND ItemID = %s""", (self.order_id, self.menu_item.item_id))
+        con.commit()
+
+    def get_total_price(self) -> float:
+        """
+        calculates subtotal for this item
+        """
+        return self.menu_item.price * self.quantity
