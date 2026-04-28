b08c8fa1f93daac7662ead59db7a04152a865526
therealsnooze <carlosdsanchez06@gmail.com>
2026-04-28 16:37:29 -0700

    Update Restaurant.py

--- yumRush/services/Restaurant.py
+++ yumRush/services/Restaurant.py
@@ -11,60 +11,6 @@ con = mysql.connector.connect(
 
 cur = con.cursor()
 
-class User:
-  """super class representing a generic system user"""
-  def __init__(self, accountID: int, name: str):
-    # user account identifier
-    self.accountID = accountID
-
-    # user display name
-    self.name = name
-
-    # indicates whether account is active
-    self.active = True
-
-    # indicates whether account is logged in
-    self.logged_in = False
-
-def sign_up(self, user_type: str) -> bool:
-  """
-  simulates account registration
-  """
-  cur.execute("""
-              INSERT INTO users (UserID, Type, Name, Active)) VALUES (%s, %s, %s, %s)
-              """, (self.accountID, user_type, self.name, self.active))
-  con.commit()
-  return True
-
-def sign_in(self) -> None:
-  """
-  simulates user logging in
-  """
-  self.logged_in = True
-
-def sign_out(self) -> None:
-  """
-  simulates user logging out
-  """
-  self.logged_in = False
-
-def delete_account(self) -> None:
-  """
-  deactivates account
-  """
-  self.active = False
-  cur.execute("""UPDATE Users SET Active = %s WHERE UserID = %s""", (False, self.accountID))
-  con.commit()
-
-def edit_profile(self, name: Optional[str] = None) -> None:
-  """
-  updates user profile information
-  """
-  if name:
-    self.name = name
-    cur.execute("""UPDATE Users SET Name = %s WHERE UserID = %s""", (self.name, self.accountID))
-    con.commit()
-
 class Restaurant(User):
   """
   represents a restaurant in the delivery system
