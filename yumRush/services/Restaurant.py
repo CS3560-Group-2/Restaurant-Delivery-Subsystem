739f6be594918b0d300ef70e60b5ffc52c43fbc4
therealsnooze <carlosdsanchez06@gmail.com>
2026-04-28 17:10:32 -0700

    Update Restaurant.py

--- yumRush/services/Restaurant.py
+++ yumRush/services/Restaurant.py
@@ -16,8 +16,8 @@ class Restaurant(User):
   represents a restaurant in the delivery system
   responsible for managing menu items and updating order preparation status
   """
-  def __init__(self, accountID: int, address: Address, name: str):
-    super().__init__(accountID, name)
+  def __init__(self, accountID: int, address: Address):
+    super().__init__(accountID)
 
     # physical restaurant location
     self.address = address
